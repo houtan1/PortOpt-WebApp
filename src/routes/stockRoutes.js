7c732e29a7ad0726c9ee1b3b5126beafdef2cb3a
houtan1 <houtan@ymail.com>
2017-05-25 22:37:31 -0700

    Forms and Get
    Using HTML forms and Get method with Express routing, the user can now
    select a pair of stocks from a list and be prompted for a bounded
    desired return, and finally be given an investment strategy. Fulfills #4

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -6,6 +6,7 @@ var stockRouter = express.Router();
 var stockExpect = require("../../lib/stockMath").expect;
 var stockRisk = require("../../lib/stockMath").risk;
 var stockCorrelation = require("../../lib/stockMath").correlate;
+var stockStrategy = require("../../lib/stockMath").strategize;
 
 // set up router function that returns stockRouter
 var router = function() {
@@ -18,52 +19,69 @@ var router = function() {
     {
       name: 'StockB',
       monthly: [3, 7, -6, 4]
+    },
+    {
+      name: 'StockC',
+      monthly: [6, 14, -12, 8]
     }
   ];
 
   // define the routes in stockRouter
-  stockRouter.route('/')
-    .get(function(req, res) {
-      res.render('stockListView', {
-        title: 'stockListView',
-        // send in our JSON array of stock info
-        stock: stocks
-      });
+  stockRouter.route('/').get(function(req, res) {
+    res.render('stockListView', {
+      title: 'stockListView',
+      // send in our JSON array of stock info
+      stock: stocks
     });
+  });
 
-  stockRouter.route('/info/:id')
-    .get(function(req, res) {
-      var id = req.params.id;
-      res.render('stockView', {
-        title: 'SingleStock',
-        // send in our single JSON stock object
-        stock: stocks[id],
-        expectation: stockExpect(stocks[id]),
-        risk: stockRisk(stocks[id])
-      });
+  stockRouter.route('/pair').get(function(req, res) {
+    var id1 = req.query.index[0];
+    var id2 = req.query.index[1];
+    var exp1 = stockExpect(stocks[id1]);
+    var exp2 = stockExpect(stocks[id2]);
+    if (exp1 <= exp2) {
+      var expMin = exp1;
+      var expMax = exp2;
+    } else {
+      var expMin = exp2;
+      var expMax = exp1;
+    }
+    res.render('pairBoundsView', {
+      title: 'pairBoundsView',
+      id1: id1,
+      id2: id2,
+      stockA: stocks[id1],
+      stockB: stocks[id2],
+      correlation: stockCorrelation(stocks[id1], stocks[id2]),
+      expMin: expMin,
+      expMax: expMax
     });
+  });
 
-  stockRouter.route('/choice/:id1')
-    .get(function(req, res) {
-      var id1 = req.params.id1;
-      res.render('stockListViewMod', {
-        title: 'stockListViewMod',
-        remove: id1,
-        stock: stocks
-      });
+  stockRouter.route('/strategy/:id1-:id2').get(function(req, res) {
+    var id1 = req.params.id1;
+    var id2 = req.params.id2;
+    var exp = req.query.value;
+    res.render('strategyView', {
+      title: 'strategyView',
+      stockA: stocks[id1],
+      stockB: stocks[id2],
+      exp: exp,
+      strategy: stockStrategy(stocks[id1], stocks[id2], exp)
     });
+  });
 
-  stockRouter.route('/choice/pair/:id1-:id2')
-    .get(function(req, res) {
-      var id1 = req.params.id1;
-      var id2 = req.params.id2;
-      res.render('pairBoundsView', {
-        title: 'pairBoundsView',
-        stockA: stocks[id1],
-        stockB: stocks[id2],
-        correlation: stockCorrelation(stocks[id1], stocks[id2])
-      });
+  stockRouter.route('/info/:id').get(function(req, res) {
+    var id = req.params.id;
+    res.render('stockView', {
+      title: 'SingleStock',
+      // send in our single JSON stock object
+      stock: stocks[id],
+      expectation: stockExpect(stocks[id]),
+      risk: stockRisk(stocks[id])
     });
+  });
 
   // individual stock route with chrome fix
   // show the expectation and risk of each stock
